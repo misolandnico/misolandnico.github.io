f5cf6c5412c6aecdb56276df34bb4caa2cf19e15
Nico Buehler <misolandnico@gmail.com>
2023-09-19 13:46:56 -0500

    fixed link bug

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # misolandnico.github.io
+
+This is the repository hosting our wedding website.
--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 window.onscroll = function () {
   scrollFunction();
+  console.log("scrolling");
 };
 
 function scrollFunction() {
